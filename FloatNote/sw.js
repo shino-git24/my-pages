65a949aee252609387acc2e72a2cce7420d06f88
shino-git24 <halash0406@gmail.com>
2025-07-02 23:32:07 +0900

    url変更

--- FloatNote/sw.js
+++ FloatNote/sw.js
@@ -1,12 +1,12 @@
 const CACHE_NAME = 'floatnote-cache-v1';
 const urlsToCache = [
-    '/',
-    '/index.html',
-    '/style.css',
-    '/script.js',
-    '/manifest.json',
-    '/icons/icon-192x192.png',
-    '/icons/icon-512x512.png'
+    './',
+    './index.html',
+    './style.css',
+    './script.js',
+    './manifest.json',
+    './icons/icon-192x192.png',
+    './icons/icon-512x512.png'
 ];
 
 // インストール時にキャッシュを追加する
